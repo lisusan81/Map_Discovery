4f779da5521885e0ee86bc33b62afa3f4d19d808
SusanLi2024 <susan.li@stonybrook.edu>
2023-09-13 00:13:30 -0400

    add func. to upload local geojson file and render map

--- src/App.js
+++ src/App.js
@@ -1,22 +1,50 @@
+import { useEffect, useState } from "react";
 import GeoMap from "./components/GeoMap";
+import { GeoJSON } from "react-leaflet";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "./App.css";
 import "leaflet/dist/leaflet.css";
 
 function App() {
+  // var geoMapInstance = <></>;
   const center = [40.902771, -73.13385];
+  const [uploadedGeoJSONFile, setUploadedGeoJSONFile] = useState(null);
+
+  function showFile(event) {
+    event.preventDefault();
+    const fileReader = new FileReader();
+    fileReader.onload = (event) => {
+      const text = JSON.parse(event.target.result);
+      setUploadedGeoJSONFile(text);
+    };
+    fileReader.readAsText(event.target.files[0]);
+  }
+
   return (
     <div>
       <h1>MAP DISCOVERY</h1>
+      <input
+        type="file"
+        accept=".json"
+        onChange={(event) => {
+          showFile(event);
+        }}
+      />
+
       <MapContainer center={center} zoom={13} scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        //TODO: add conditions such that depending on the uploaded file format,
+        {/* TODO: add conditions such that depending on the uploaded file format,
         create a map component accordingly //currently, always creating a
-        GeoJSON Map
-        <GeoMap />
+        GeoJSON Map */}
+
+        {/* <GeoMap file={uploadedGeoJSONFile} /> */}
+        {/* {geoMapInstance} */}
+        {uploadedGeoJSONFile?.features && (
+          <GeoJSON data={uploadedGeoJSONFile.features} />
+        )}
       </MapContainer>
     </div>
   );
--- src/components/GeoMap.js
+++ src/components/GeoMap.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 import { GeoJSON } from "react-leaflet";
 import na_countries from "./../na_countries";
 
-const GeoMap = () => {
+const GeoMap = (file) => {
   //TODO: remove na_countries and edit data attribute after select and upload files has been completed
-  return <GeoJSON data={na_countries.features} />;
-};
+  console.log("in Geomap, file=", file);
 
+  return <GeoJSON data={file?.features} />;
+};
+GeoMap.propTypes = {
+  file: PropTypes.object.isRequired,
+};
 export default GeoMap;
